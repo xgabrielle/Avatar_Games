f7ee8edb1d05a9069975716a6bd197a2e712d0b6
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2025-03-06 15:47:42 +0100

    moved marker movement to new script and both sides can now move

--- Assets/Checkers/CheckerGame.cs
+++ Assets/Checkers/CheckerGame.cs
@@ -4,47 +4,21 @@ using UnityEngine;
 
 public class CheckerGame : MonoBehaviour
 {
-    private Vector3 targetPosition;
-    private Vector3 markerPos;
+    internal Vector3 targetPosition;
+    internal Vector3 markerPos;
     [SerializeField] private Material yellow;
     [SerializeField] private Material blue;
     [SerializeField] private Material originalColor;
-    private GameObject marker;
+    internal GameObject marker;
     private GameObject previousMarker;
+    private MarkerMovement _markerMovement;
+    private bool currentPlayer;
+
+    private void Start()
+    {
+        _markerMovement = GetComponent<MarkerMovement>();
+    }
     
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-
-    }
-
-    void AvailableMove(GameObject square)
-    {
-        
-        if (DiagonalMove(markerPos, targetPosition))
-        {
-            Debug.Log($"TargetPos: {targetPosition}");
-            Renderer chooseColor = square.GetComponent<Renderer>();
-            chooseColor.material = yellow;
-
-            if (targetPosition.z > markerPos.z)
-            { 
-                NewMarkerPos(targetPosition);
-            }
-            
-        } else
-            Debug.Log("Not a possible move");
-
-      
-    }
-
-    void NewMarkerPos(Vector3 newMarkerPos)
-    {
-        markerPos = newMarkerPos;
-        marker.transform.position = markerPos + new Vector3(0,0.5f,0);
-    }
-
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
@@ -54,18 +28,35 @@ public class CheckerGame : MonoBehaviour
 
             if (Physics.Raycast(ray, out hit))
             {
+                if (hit.collider.CompareTag("DarkMarker"))
+                {
+                    currentPlayer = false;
+                    markerPos = hit.collider.transform.position;
+                    marker = hit.collider.gameObject;
+
+                }
+                
                 if (hit.collider.CompareTag("WhiteMarker"))
                 {
+                    currentPlayer = true;
                     markerPos = hit.collider.transform.position;
                     marker = hit.collider.gameObject;
                     PlayingMarker(marker);
 
                 }
                 
+                
                 if (hit.collider.CompareTag("BoardSquare"))
                 {
                     targetPosition = hit.collider.transform.position;
-                    AvailableMove(hit.collider.gameObject);
+                    if (currentPlayer == false)
+                    {
+                        _markerMovement.DarkMarkerMove();
+                    }
+                    else
+                    { 
+                        _markerMovement.WhiteMarkerMove();
+                    }
                     
                 }
                 
@@ -75,16 +66,6 @@ public class CheckerGame : MonoBehaviour
         
     }
     
-    internal static bool DiagonalMove(Vector3 startPos, Vector3 targetPos)
-    {
-        float newPosX = Mathf.Abs(targetPos.x-startPos.x);
-        float newPosZ = Math.Abs(targetPos.z-startPos.z);
-        
-        if (Mathf.Approximately(newPosX,1) && Mathf.Approximately(newPosZ,1)) 
-            return Mathf.Approximately(newPosX, newPosZ);
-        return false;
-
-    }
 
     void PlayingMarker(GameObject marker)
     {
--- Assets/Checkers/MarkerMovement.cs
+++ Assets/Checkers/MarkerMovement.cs
@@ -1,6 +1,57 @@
+using System;
 using UnityEngine;
 
 public class MarkerMovement : MonoBehaviour
 {
+    private CheckerGame _checkerGame;
+
+    private void Start()
+    {
+        _checkerGame = GetComponent<CheckerGame>();
+    }
+
+    internal void DarkMarkerMove()
+    {
+        if (DiagonalMove(_checkerGame.markerPos, _checkerGame.targetPosition))
+        {
+
+            if (_checkerGame.targetPosition.z < _checkerGame.markerPos.z)
+            { 
+                NewMarkerPos(_checkerGame.targetPosition);
+            }
+            
+        } else
+            Debug.Log("Not a possible move");
+        
+    }
     
+    internal void WhiteMarkerMove()
+    {
+        if (DiagonalMove(_checkerGame.markerPos, _checkerGame.targetPosition))
+        {
+            if (_checkerGame.targetPosition.z > _checkerGame.markerPos.z)
+            { 
+                NewMarkerPos(_checkerGame.targetPosition);
+            }
+            
+        } else
+            Debug.Log("Not a possible move");
+    }
+    
+    internal void NewMarkerPos(Vector3 newMarkerPos)
+    {
+        _checkerGame.markerPos = newMarkerPos;
+        _checkerGame.marker.transform.position = _checkerGame.markerPos + new Vector3(0,0.5f,0);
+    }
+
+    internal static bool DiagonalMove(Vector3 startPos, Vector3 targetPos)
+    {
+        float newPosX = Mathf.Abs(targetPos.x - startPos.x);
+        float newPosZ = Mathf.Abs(targetPos.z - startPos.z);
+
+        if (Mathf.Approximately(newPosX, 1) && Mathf.Approximately(newPosZ, 1))
+            return Mathf.Approximately(newPosX, newPosZ);
+        return false;
+
+    }
 }
