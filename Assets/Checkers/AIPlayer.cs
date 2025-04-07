e79141197f7689aa4de9719888ebd13183a06bc6
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2025-04-07 12:43:32 +0200

    removed unnecessary code

--- Assets/Checkers/AIPlayer.cs
+++ Assets/Checkers/AIPlayer.cs
@@ -22,9 +22,6 @@ public class AIPlayer : MonoBehaviour
     }
     void EasyRandomMove()
     {
-        //bool validMove = false;
-        //GameObject validPawn = null;
-        //Vector3 validStartPos = default;
         GameObject[] darkPawns = GameObject.FindGameObjectsWithTag("DarkMarker");
         foreach (GameObject pawn in darkPawns)
         {
@@ -38,32 +35,10 @@ public class AIPlayer : MonoBehaviour
                     pawn.transform.position = moveResult.LandingPos;
                     if (moveResult.CapturedPawn != null)
                         Destroy(moveResult.CapturedPawn);
-                        
-                    /*if (MarkerMovement.Movement.FreeJumpSpace(pawnPos, move).Item1)
-                        {
-                            if (MarkerMovement.Movement.Jump(pawn, pawnPos, MarkerMovement.Movement.FreeJumpSpace(pawnPos, move).Item2))
-                            {
-                                pawn.transform.position = MarkerMovement.Movement.FreeJumpSpace(pawnPos, move).Item2;
-                                validMove = true;
-                                break;
-                            }
-                            if (MarkerMovement.Movement.GetMarkerMove(pawn, pawnPos, move)) // check if square is occupied
-                            {
-                                pawn.transform.position = move;
-                                validMove = true;
-                                break;
-                            }
-                        }
-                        else if (MarkerMovement.Movement.GetMarkerMove(pawn, pawnPos, move)) // check if square is occupied
-                        {
-                            pawn.transform.position = move;
-                            validMove = true;
-                            break;
-                        }*/
                     MarkersGenerator.instance.UpdatePawns(pawn, pawnPos, moveResult.LandingPos );
                     GameStateManager.instance.LastMove(pawn, pawnPos, moveResult.LandingPos);
                     if (_checkerGame.HasGameOver(_checkerGame.isAiTurn ? "WhiteMarker" : "DarkMarker"))
-                    {
+                    { 
                         _checkerGame.isGameOver = true;
                         Debug.Log("Game over, AI lost (No valid moves)");
                         ChatManager.Instance.SendMessageToAI("");
