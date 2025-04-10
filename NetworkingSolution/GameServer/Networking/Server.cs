4b40bdd6e29435457b0e9237042c65cf10c1b76b
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2025-04-10 11:57:30 +0200

    create server

---
+++ NetworkingSolution/GameServer/Networking/Server.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace GameServer.Networking;
+
+public class Server
+{
+    private TcpListener _listener;
+    private List<Client> _clients = new();
+    private int port = 3030;
+
+    public void OnStart()
+    {
+        _listener = new TcpListener(IPAddress.Any, port);
+        _listener.Start();
+        Console.WriteLine($"Server start on Port {port}");
+
+        while (true)
+        {
+            TcpClient tcpClient = _listener.AcceptTcpClient();
+            Console.WriteLine("Client is connected");
+            Client clientHandler = new Client(tcpClient, this);
+            if (_clients.Count < 3) _clients.Add(clientHandler);
+            new Thread(clientHandler.Handle).Start();
+        }
+    }
+
+    public void Broadcast(string message, Client excludeClient = null)
+    {
+        foreach (var client in _clients)
+        {
+            if (client != excludeClient)
+                client.Send(message);
+        }
+    }
+
+    public void RemoveClient(Client client)
+    {
+        _clients.Remove(client);
+        Console.WriteLine("Client disconnected");
+    }
+}
